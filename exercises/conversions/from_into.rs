dfd77827e221fe20a4bc555b4665e6aae328bc70
qdu <onlylinker@126.com>
2024-04-13 14:39:16 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,21 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if let Some((name, age)) = s.split_once(',') {
+            let name = name.trim();
+            if name.len() > 0 {
+                if let Ok(age) = age.parse::<usize>() {
+                    return Person {
+                        name: String::from(name),
+                        age,
+                    };
+                }
+            }
+        }
+
+        Person::default()
     }
 }
 
